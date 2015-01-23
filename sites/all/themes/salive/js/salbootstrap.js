4f609d404fa714ce9b3e6b62f4dd4f017a5d6b79
Dylan Underwood <dylan@hydeinteractive.com>
2015-01-23 16:47:17 -0500

    end of day 1-23-15

--- sites/all/themes/salive/js/salbootstrap.js
+++ sites/all/themes/salive/js/salbootstrap.js
@@ -6,14 +6,16 @@
       $('.death-notice').click(function() {
         $(this).children('.death-notice-controls').toggle();
       });
-      
-      $('.event-sale').equalHeights();
-      $('.view-id-events .thumbnail').equalHeights();
-      $('.view-obituaries .obit').equalHeights();
-      $('.view-newsthumbs li.col-md-2').equalHeights();
-      $('#block-views-news-thumbs-block li.views-row').equalHeights();
-      $('.view-id-News .views-row').equalHeights();
-      $('#editorspick .view-content, #editorspick .views-row').equalHeights();
+      $(document).ready(function(){
+        $('.event-sale').equalHeights();
+        $('.view-id-events .thumbnail').equalHeights();
+        $('.view-obituaries .obit').equalHeights();
+        $('.view-newsthumbs li.col-md-2').equalHeights();
+        $('#block-views-news-thumbs-block li.views-row').equalHeights();
+        $('.view-id-News .views-row').equalHeights();
+        $('#editorspick .view-content, #editorspick .views-row').equalHeights();
+      });
+
     }
   };
 })(jQuery);
