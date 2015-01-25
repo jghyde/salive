906d542fe4980e49b56bbc527e600b50278c5d73
Dylan Underwood <dylan@hydeinteractive.com>
2015-01-25 14:18:08 -0500

    waiting for the image module now joe

--- sites/all/themes/salive/templates/views/views-view-list--News.tpl.php
+++ sites/all/themes/salive/templates/views/views-view-list--News.tpl.php
@@ -13,7 +13,7 @@
     <div class="views-row-<?php print $id+1; ?>">
       <li class="<?php print $classes_array[$id]; ?>"><?php print $row; ?></li>
     </div>
-		<?php $i++; if ($i == $wrapper || $id == $last_row) print '</div><div class="col-md-12 news-insert"><div class="ads"><a><img src="/sites/default/files/930x180.png"></a></div></div>'; ?>
+		<?php $i++; if ($i == $wrapper || $id == $last_row) print '</div><div class="col-md-12 news-insert"><div class="ads"><script type="text/javascript">broadstreet.zone(40354);</script></div></div>'; ?>
 	<?php endforeach; ?>
   </ul>
 <?php print $wrapper_suffix; ?>
