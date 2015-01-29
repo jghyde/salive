24d2450a0866b329c83a52f82506714b4a090ceb
Dylan Underwood <dylan@hydeinteractive.com>
2015-01-29 18:21:12 -0500

    end of day 1-29-15

--- sites/all/themes/salive/templates/node--article.tpl.php
+++ sites/all/themes/salive/templates/node--article.tpl.php
@@ -1,19 +1,13 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-	<?php print $byline; ?>
-	<?php if ($num_revisions < '1'): ?>
-	<br />Last Updated: <?php echo date('M j, Y g:i a', $node->changed); ?>
-	<?php endif; ?>
-	</p>
 	
-  <?php
-    // Hide comments, tags, and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    hide($content['field_tags']);
-    print render($content);
-  ?>
+	<?php print render($content['field_image']); ?>
 	
-	<?php if (!empty($content['field_tags'])): ?>
+	<div class="node-author">
+		<?php print $byline; ?>
+	</div>
+	
+	<?php print render($content['body']); ?>
+	
   <footer>
     <?php print render($content['field_tags']); ?>
     <?php if ($display_submitted): ?>
@@ -22,7 +16,6 @@
     </div>
     <?php endif; ?>
   </footer>
-  <?php endif; ?>
   
   <?php
   if (!empty($sell)) {
