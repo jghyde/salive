b09043a195620aa4f67d3ac9203d991f6341bf80
Dylan Underwood <dylan@hydeinteractive.com>
2015-02-02 19:40:02 -0500

    small changes to fix java problem

--- sites/all/themes/salive/js/salbootstrap.js
+++ sites/all/themes/salive/js/salbootstrap.js
@@ -2,16 +2,14 @@
 (function ($) {
   Drupal.behaviors.salivebootstrap = {
     attach: function (context, settings) {
-      $(document).ready(function(){
-        //$('.event-sale').equalHeights();
-        $('.view-id-events .thumbnail').equalHeights();
-        $('.view-obituaries .obit').equalHeights();
-        $('.view-births .births').equalHeights();
-        $('.view-newsthumbs li.col-md-2').equalHeights();
-        $('#block-views-news-thumbs-block li.views-row').equalHeights();
-        $('.view-id-News .views-row').equalHeights();
-        $('#editorspick .view-content, #editorspick .views-row').equalHeights();
-      });
+      $('.event-sale').equalHeights();
+      $('.view-id-events .thumbnail').equalHeights();
+      $('.view-obituaries .obit').equalHeights();
+      $('.view-births .births').equalHeights();
+      $('.view-newsthumbs li.col-md-2').equalHeights();
+      $('#block-views-news-thumbs-block li.views-row').equalHeights();
+      $('.view-id-News .views-row').equalHeights();
+      $('#editorspick .view-content, #editorspick .views-row').equalHeights();
       // colorbox
       $('img.imgbody').click(function() {
         $(this).colorbox({href: $(this).attr('src')});
