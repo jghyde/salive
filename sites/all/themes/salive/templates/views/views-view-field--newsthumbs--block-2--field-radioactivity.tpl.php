10c7be549ca08c78ff067df5597f2a9ad9c0082f
Dylan Underwood <dylan@hydeinteractive.com>
2015-02-18 14:55:10 -0500

    radio tpl

---
+++ sites/all/themes/salive/templates/views/views-view-field--newsthumbs--block-2--field-radioactivity.tpl.php
@@ -0,0 +1,10 @@
+<?php
+if ($field->original_value == 0 || $field->original_value == '') {
+  $fields['field_radioactivity']->content = 'Cool';
+}
+elseif ((int)$field->original_value > 10000) {
+  $field->original_value = (int)$field->original_value/10000;
+  $field->original_value = $field->original_value . ' x 10<sup>4</sup>';
+}
+?>
+<div class="text-center"><h4><small><i class="glyphicons fire"></i><?php print $field->original_value; ?></small></h4></div>
