c24cbc47b97f6665818e9336654dcc62ed48897f
Dylan Underwood <dylanunderwood@Dylans-iMac.local>
2015-03-31 13:03:44 -0400

    maintenance theme

--- sites/all/themes/salive/templates/maintenance-page.tpl.php
+++ sites/all/themes/salive/templates/maintenance-page.tpl.php
@@ -1,65 +1,38 @@
-<?php
-/**
- * @file
- * Implementation to display a single Drupal page while offline.
- *
- * All the available variables are mirrored in page.tpl.php.
- *
- * @see template_preprocess()
- * @see template_preprocess_maintenance_page()
- * @see bartik_process_maintenance_page()
- */
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>">
 <head>
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
+  <title>Performing Maintenance | San Angelo LIVE!</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="/sites/all/themes/salive/css/maintenance.css" rel="stylesheet" type="text/css">
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  <div id="header">
+	<div class="section">
+	  <div id="site-logo-and-name">
+		<?php if ($logo): ?>
+		  <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" />
+		<?php endif; ?>
+	  </div> <!-- /#site-logo-and-name -->
+	</div> <!-- /.section -->
+  </div> <!-- /#header -->
+  
+  <div id="page-wrapper">
+    <div id="page">
+      <div id="main">
+        <?php print $messages; ?>
+        <div id="content">
+          <?php print render($page['help']); ?>
+          <?php print $content; ?>
+        </div> <!-- /#content -->
+      </div> <!-- /#main -->
+    </div> <!-- /#page -->
+  </div> <!-- /#page-wrapper -->
+  <div id="footer">
+    <div id="footerleft">
+      San Angelo LIVE! &bull; Hyde Interactive <br /> &copy; <?php print date('Y'); ?> Copyright &bull; All Rights Reserved
+    </div>
   </div>
-
-  <div id="page-wrapper"><div id="page">
-
-    <div id="header"><div class="section clearfix">
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-          <?php if ($site_name): ?>
-            <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong>
-            </div>
-          <?php endif; ?>
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-              <?php print $site_slogan; ?>
-            </div>
-          <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
-      <?php endif; ?>
-    </div></div> <!-- /.section, /#header -->
-
-    <div id="main-wrapper"><div id="main" class="clearfix">
-      <div id="content" class="column"><div class="section">
-        <a id="main-content"></a>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print $content; ?>
-        <?php if ($messages): ?>
-          <div id="messages"><div class="section clearfix">
-            <?php print $messages; ?>
-          </div></div> <!-- /.section, /#messages -->
-        <?php endif; ?>
-      </div></div> <!-- /.section, /#content -->
-    </div></div> <!-- /#main, /#main-wrapper -->
-
-  </div></div> <!-- /#page, /#page-wrapper -->
 
 </body>
 </html>
