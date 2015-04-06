09cdda3fd12592ee140c2b92cc1e559a4eb65a42
Dylan Underwood <dylanunderwood@Dylans-iMac.local>
2015-04-06 11:46:02 -0400

    core update and patches

--- sites/all/themes/salive/js/salbootstrap.js
+++ sites/all/themes/salive/js/salbootstrap.js
@@ -2,21 +2,13 @@
 (function ($) {
   Drupal.behaviors.salivebootstrap = {
     attach: function (context, settings) {
-	      $('.event-sale').equalHeights();
-	      $('.view-id-events .thumbnail').equalHeights();
-	      $('#block-views-community-block_1 .thumbnail').equalHeights();
-	      $('.view-obituaries .obit').equalHeights();
-	      $('.view-births .births').equalHeights();
-	      $('#block-views-newsthumbs-block_1 li.col-md-2').equalHeights();
-	      $('#block-views-news-thumbs-block li.views-row').equalHeights();
-	      $('.view-id-News .views-row').equalHeights();
-	      $('#block-views-community-block_2 .obit').equalHeights();
       // colorbox
       $('img.imgbody').click(function() {
         $(this).colorbox({href: $(this).attr('src')});
       });
       //$('img.imgbody').colorbox({maxWidth: '90%'});
       //$('#nodeSlide img').colorbox({maxWidth: '90%'});
+      
       $('#nodeSlide').each(function(){
         if( $('#nodeSlide').find("div").length > 1 ) $('#nodeSlide').owlCarousel({
           items:1,
@@ -76,7 +68,7 @@
         nav:false,
         items:3,
       });
-      $("#newsHot, #newsRant, #newsEdit, #newsPaid").owlCarousel({
+      $("#newsHot").owlCarousel({
         loop:false,
         margin:10,
         nav:true,
@@ -99,6 +91,76 @@
                 "<i class='owl-next fa fa-arrow-right'></i>"
             ],
       });
+      $("#newsRant").owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        //autoplay:true,
+        //autoplayTimeout:2000,
+        //autoplayHoverPause:true,
+        responsive:{
+          0:{
+            items:1
+          },
+          600:{
+            items:3
+          },
+          1000:{
+            items:5
+          }
+        },
+        navText: [
+                "<i class='owl-prev fa fa-arrow-left'></i>",
+                "<i class='owl-next fa fa-arrow-right'></i>"
+            ],
+      });
+      $("#newsEdit").owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        //autoplay:true,
+        //autoplayTimeout:2000,
+        //autoplayHoverPause:true,
+        responsive:{
+          0:{
+            items:1
+          },
+          600:{
+            items:3
+          },
+          1000:{
+            items:5
+          }
+        },
+        navText: [
+                "<i class='owl-prev fa fa-arrow-left'></i>",
+                "<i class='owl-next fa fa-arrow-right'></i>"
+            ],
+      });
+      $("#newsPaid").owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        //autoplay:true,
+        //autoplayTimeout:2000,
+        //autoplayHoverPause:true,
+        responsive:{
+          0:{
+            items:1
+          },
+          600:{
+            items:3
+          },
+          1000:{
+            items:5
+          }
+        },
+        navText: [
+                "<i class='owl-prev fa fa-arrow-left'></i>",
+                "<i class='owl-next fa fa-arrow-right'></i>"
+            ],
+      });
+
     }
   };
 })(jQuery);
