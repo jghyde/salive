76cd31a3d3724e346d0c015693499d0d2d5195c2
Dylan Underwood <dylanunderwood@Dylans-iMac.local>
2015-04-21 16:54:08 -0400

    feature updates

--- sites/all/themes/salive/templates/field--field-image.tpl.php
+++ sites/all/themes/salive/templates/field--field-image.tpl.php
@@ -30,6 +30,15 @@
 				</div>
 	    <?php endforeach; ?>
 		  </div>
+		<?php elseif ($element['#bundle'] == 'event_free'): ?>
+		  <div id="nodeSlide" class="owl-carousel">
+			<?php foreach ($items as $delta => $item): ?>
+				<div class="item">
+					<?php print render($item); ?>
+					<p class="caption"><?php print $item['#item']['alt']; ?></p>
+				</div>
+	    <?php endforeach; ?>
+		  </div>
 		<?php else: ?>
 		<?php foreach ($items as $delta => $item): ?>
 				<?php print render($item); ?>
