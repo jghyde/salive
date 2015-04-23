c15ef413b77e5085a2e3557dbfad288770aa4139
Dylan Underwood <dylanunderwood@Dylans-iMac.local>
2015-04-23 11:34:45 -0400

    event style changes

--- sites/all/themes/salive/templates/field--field-image.tpl.php
+++ sites/all/themes/salive/templates/field--field-image.tpl.php
@@ -10,39 +10,20 @@
 				<?php print render($item); ?>
 				<p class="caption"><?php print $item['#item']['alt']; ?></p>
 			</div>
-    <?php endforeach; ?>
+		<?php endforeach; ?>
 	  </div>
-		<?php elseif ($element['#bundle'] == 'event_paid'): ?>
-		  <div id="nodeSlide" class="owl-carousel">
-			<?php foreach ($items as $delta => $item): ?>
-				<div class="item">
-					<?php print render($item); ?>
-					<p class="caption"><?php print $item['#item']['alt']; ?></p>
-				</div>
-	    <?php endforeach; ?>
-		  </div>
-		<?php elseif ($element['#bundle'] == 'event'): ?>
-		  <div id="nodeSlide" class="owl-carousel">
-			<?php foreach ($items as $delta => $item): ?>
-				<div class="item">
-					<?php print render($item); ?>
-					<p class="caption"><?php print $item['#item']['alt']; ?></p>
-				</div>
-	    <?php endforeach; ?>
-		  </div>
-		<?php elseif ($element['#bundle'] == 'event_free'): ?>
-		  <div id="nodeSlide" class="owl-carousel">
-			<?php foreach ($items as $delta => $item): ?>
-				<div class="item">
-					<?php print render($item); ?>
-					<p class="caption"><?php print $item['#item']['alt']; ?></p>
-				</div>
-	    <?php endforeach; ?>
-		  </div>
-		<?php else: ?>
-		<?php foreach ($items as $delta => $item): ?>
-				<?php print render($item); ?>
-    <?php endforeach; ?>
+	  <?php elseif ($element['#bundle'] == 'event_paid' || 'event' || 'event_free'): ?>
+		<div id="nodeSlide" class="owl-carousel">
+		  <?php foreach ($items as $delta => $item): ?>
+			  <div class="item">
+				  <?php print render($item); ?>
+			  </div>
+		  <?php endforeach; ?>
+		</div>
+	  <?php else: ?>
+	  <?php foreach ($items as $delta => $item): ?>
+		<?php print render($item); ?>
+	  <?php endforeach; ?>
 	<?php endif; ?>
   </div>
 </div>
