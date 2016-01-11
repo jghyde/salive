6e1c715c5d99b081ae15978a11e651e0e3937b26
Joe Hyde <joeh@hydeinteractive.com>
2016-01-11 01:24:00 -0600

    adds js insterstitual

--- sites/all/themes/salive/templates/html.tpl.php
+++ sites/all/themes/salive/templates/html.tpl.php
@@ -13,6 +13,28 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
   <?php print $scripts; ?>
+  <?php
+  if ($_GET['q'] == 'node') {
+    $dest = '/';
+  }
+  elseif ($_GET['q'] == 'welcome') {
+    $dest = '/';
+  }
+  else {
+    $dest = $_GET['q'];
+  }
+  $length = 10;
+  $randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+  ?>
+  <script>
+(function($){
+    if ($.cookie('welcome_page') == null) {
+      window.setTimeout(function(){
+        window.location.href = '/welcome?destination=<?php print $dest; ?>?b=<?php print $randomString; ?>';
+      },0);
+    }
+})(jQuery);
+  </script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php print $page_top; ?>
