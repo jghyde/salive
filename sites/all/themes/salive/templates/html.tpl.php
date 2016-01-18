a7237870274ac8d2d5e7527ca05de5e4c2b42797
Joe Hyde <joeh@hydeinteractive.com>
2016-01-18 12:34:54 -0600

    beavers ad

--- sites/all/themes/salive/templates/html.tpl.php
+++ sites/all/themes/salive/templates/html.tpl.php
@@ -28,10 +28,25 @@
   ?>
   <script>
 (function($){
+    function hasCookies() {
+      return (navigator.cookieEnabled);
+    }
     if ($.cookie('welcome_page') == null) {
-      window.setTimeout(function(){
-        window.location.href = '/welcome?destination=<?php print $dest; ?>?b=<?php print $randomString; ?>';
-      },0);
+      if (hasCookies() == true) {
+        //code
+        window.setTimeout(function(){
+          window.location.href = '/welcome?destination=<?php print $dest; ?>?b=<?php print $randomString; ?>';
+        },0);
+      }
+      else {
+        if (document.referrer.indexOf('sanangelolive.com') >= 0) {
+        }
+        else {
+          window.setTimeout(function(){
+            window.location.href = '/welcome?destination=<?php print $dest; ?>?b=<?php print $randomString; ?>';
+          },0);          
+        }
+      }
     }
 })(jQuery);
   </script>
@@ -40,5 +55,20 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1092781257421142');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1092781257421142&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
 </body>
 </html>
