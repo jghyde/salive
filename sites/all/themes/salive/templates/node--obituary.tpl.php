69dc77d5b0e50301122252109912c4a409e87ef2
Joe Hyde <joeh@hydeinteractive.com>
2016-07-27 12:38:11 -0500

    media.net changes

--- sites/all/themes/salive/templates/node--obituary.tpl.php
+++ sites/all/themes/salive/templates/node--obituary.tpl.php
@@ -26,6 +26,24 @@ else:
     <footer>
       <?php print render($content['field_tags']); ?>
       <?php print render($content['links']); ?>
+        <div id="medianet">
+            <script id="mNCC" language="javascript">
+                var w = document.getElementById("medianet").offsetWidth;
+                var bannerwidth = 300;
+                if (w > 599) {
+                    bannerwidth=600;
+                }
+                medianet_width = bannerwidth;
+                medianet_height = "250";
+                medianet_crid = "148244274";
+                medianet_versionId = "111299";
+                (function() {
+                    var isSSL = 'https:' == document.location.protocol;
+                    var mnSrc = (isSSL ? 'https:' : 'http:') + '//contextual.media.net/nmedianet.js?cid=8CUFP6TTD' + (isSSL ? '&https=1' : '');
+                    document.write('<scr' + 'ipt type="text/javascript" id="mNSC" src="' + mnSrc + '"></scr' + 'ipt>');
+                })();
+            </script>
+        </div>
     </footer>
   <?php endif; ?>
   <?php print render($content['comments']); ?>
