a3745de6e17f9eb828ea7ad3c9142e1d8212009a
Joe Hyde <joeh@hydeinteractive.com>
2016-08-17 09:44:25 -0500

    removes media.net footer

--- sites/all/themes/salive/templates/node--article.tpl.php
+++ sites/all/themes/salive/templates/node--article.tpl.php
@@ -33,25 +33,6 @@
 		<?php if (!empty($sell)) {
 			print $sell;
 		} ?>
-    <div id="medianet">
-      <script id="mNCC" language="javascript">
-        var w = document.getElementById("medianet").offsetWidth;
-        var bannerwidth = 300;
-        if (w > 599) {
-          bannerwidth=600;
-        }
-        medianet_width = bannerwidth;
-        medianet_height = "250";
-        medianet_crid = "148244274";
-        medianet_versionId = "111299";
-        (function() {
-          var isSSL = 'https:' == document.location.protocol;
-          var mnSrc = (isSSL ? 'https:' : 'http:') + '//contextual.media.net/nmedianet.js?cid=8CUFP6TTD' + (isSSL ? '&https=1' : '');
-          document.write('<scr' + 'ipt type="text/javascript" id="mNSC" src="' + mnSrc + '"></scr' + 'ipt>');
-        })();
-      </script>
-    </div>
-
   </footer>
 
   <div id="comments">
