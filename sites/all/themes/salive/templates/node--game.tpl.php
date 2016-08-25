a39d2c436e64744ad700cd23e9d0fc68f052eeab
Joe Hyde <joeh@hydeinteractive.com>
2016-08-25 17:09:19 -0500

    football!

--- sites/all/themes/salive/templates/node--game.tpl.php
+++ sites/all/themes/salive/templates/node--game.tpl.php
@@ -41,29 +41,58 @@ else {
 </div>
 <?php } ?>
 <?php endif; ?>
+  <?php
+  $quarter = $content['field_game_quarter'][0]['#markup'];
+  if (empty($content['field_game_quarter'][0]['#markup']) || !isset($content['field_game_quarter'][0]['#markup'])) {
+    $quarter = 'Before Kickoff';
+  }
+  ?>
+
 <table class="table table-striped">
 <tr>
-<th class="game-team">Team (<?php print $content['field_game_quarter'][0]['#markup']; ?>)<?php print  ' ' . $evrybit; ?></th>
+<th class="game-team">Team (<?php print $quarter; ?>)<?php print  ' ' . $evrybit; ?></th>
 <?php if ($content['field_final_score'][0]['#markup'] == 1) {
   $tclass= 'success';
 }
 else {
   $tclass = 'info';
 }
+
+  //create the ad slots
+  $ad_visitor = strtolower($content['field_visiting_team'][0]['#markup']);
+  $ad_home = strtolower($content['field_home_team'][0]['#markup']);
+  $ad_visitor = preg_replace('/\s+/', '', $ad_visitor);
+  $ad_home = preg_replace('/\s+/', '', $ad_home);
 ?>
 <th class="<?php print $tclass; ?>"><?php print $total; ?></th>
 </tr>
 <tr>
-<td class="game-visitor"><?php print $content['field_visiting_team'][0]['#markup']; ?></td>
+<td class="game-visitor"><?php print $content['field_visiting_team'][0]['#markup']; ?> (Visitor)</td>
 <td class="<?php print $tclass; ?>" width="30%"><?php print (($vscore == '' || $vscore === 0) ? '0' :$vscore); ?></td
 </tr>
 <tr>
-<td class="game-home"><?php print $content['field_home_team'][0]['#markup']; ?></td>
+<td class="game-home"><?php print $content['field_home_team'][0]['#markup']; ?> (Home)</td>
 <td class="<?php print $tclass; ?>" width="30%"><?php print (($hscore == '' || $hscore === 0) ? '0' :$hscore); ?></td>
 </tr>
+  <tr>
+    <td colspan="2">
+      <div class="football-home pull-left">
+        <?php
+        $block = module_invoke('dfp', 'block_view', 'fb_' . $ad_home);
+        print render($block['content']);
+        ?>
+      </div>
+      <div class="football-visitor pull-left">
+        <?php
+        $block = module_invoke('dfp', 'block_view', 'fb_' . $ad_visitor);
+        print render($block['content']);
+        ?>
+      </div>
+    </td>
+  </tr>
 </table>
 <div class="status">
-Last revised on <?php print date('M j, Y \a\t h:i', $node->revision_timestamp) . ' '; ?>
+This score last updated at <?php print date('M j, Y \a\t h:i', $node->revision_timestamp) . ' '; ?>
 <?php if ($is_admin): ?>
 <?php
 $revision_author = user_load($revision_uid);
