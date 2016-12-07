e90d004200cd44fd7753f069866f655253fd9467
Joe Hyde <joeh@hydeinteractive.com>
2016-12-06 21:38:10 -0600

    ezoic

--- sites/all/themes/salive/templates/node--obituary.tpl.php
+++ sites/all/themes/salive/templates/node--obituary.tpl.php
@@ -27,22 +27,9 @@ else:
       <?php print render($content['field_tags']); ?>
       <?php print render($content['links']); ?>
         <div id="medianet">
-            <script id="mNCC" language="javascript">
-                var w = document.getElementById("medianet").offsetWidth;
-                var bannerwidth = 300;
-                if (w > 599) {
-                    bannerwidth=600;
-                }
-                medianet_width = bannerwidth;
-                medianet_height = "250";
-                medianet_crid = "148244274";
-                medianet_versionId = "111299";
-                (function() {
-                    var isSSL = 'https:' == document.location.protocol;
-                    var mnSrc = (isSSL ? 'https:' : 'http:') + '//contextual.media.net/nmedianet.js?cid=8CUFP6TTD' + (isSSL ? '&https=1' : '');
-                    document.write('<scr' + 'ipt type="text/javascript" id="mNSC" src="' + mnSrc + '"></scr' + 'ipt>');
-                })();
-            </script>
+            <!-- Ezoic - Story Bottom - long_content -->
+            <div id="ezoic-pub-ad-placeholder-100"></div>
+            <!-- End Ezoic - Story Bottom - long_content -->
         </div>
     </footer>
   <?php endif; ?>
