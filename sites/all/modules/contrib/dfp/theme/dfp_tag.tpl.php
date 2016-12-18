1677c45d44e2518d841680f882ccf3362f2ea6bc
Joe Hyde <joeh@hydeinteractive.com>
2016-12-18 11:05:34 -0600

    dfp mods for ezoic

--- sites/all/modules/contrib/dfp/theme/dfp_tag.tpl.php
+++ sites/all/modules/contrib/dfp/theme/dfp_tag.tpl.php
@@ -9,9 +9,19 @@
   <?php if (isset($slug)):
     print drupal_render($slug);
   endif; ?>
+  <?php
+  if (!empty($variables['ezoic_wrapper']['#prefix'])) {
+      print $variables['ezoic_wrapper']['#prefix'];
+  }
+  ?>
   <script type="text/javascript">
     googletag.cmd.push(function() {
       googletag.display("<?php print $tag->placeholder_id ?>");
     });
   </script>
+  <?php
+  if(!empty($variables['ezoic_wrapper']['#prefix']) && !empty($variables['ezoic_wrapper']['#suffix'])) {
+      print $variables['ezoic_wrapper']['#suffix'];
+  }
+  ?>
 </div>
